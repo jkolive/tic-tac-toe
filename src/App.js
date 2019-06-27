afdfe0354c232a52331b649e2f96ad094cb8ea7c
Jackson de Oliveira <mr.jkolive@gmail.com>
2019-06-26 21:00:03 -0300

    Upload dos arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import Header from './components/Header/index';
+import Main from './components/Main/index';
+
+const App = () => (
+  <div className="App">
+  <Header/>
+  <Main/>
+</div>
+);
+
+export default App;
